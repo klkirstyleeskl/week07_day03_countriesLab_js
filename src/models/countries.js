a00d935347e9ab2e847d879d8e4f93d2e7c702bf
klkirstyleeskl <k.lees@btinternet.com>
2019-01-16 14:21:19 +0000

    publishes selected country

--- src/models/countries.js
+++ src/models/countries.js
@@ -7,21 +7,23 @@ const Country = function (){
 }
 
 Country.prototype.getData = function () {
-    const request = new RequestHelper('https://restcountries.eu/rest/v2/all');
+  const request = new RequestHelper('https://restcountries.eu/rest/v2/all');
     request.get((data) => {
     this.text = data
-    PubSub.publish('Country:country-loaded', this.text)
+
+  PubSub.publish('Country:country-loaded', this.text)
     console.log(data);
+  PubSub.subscribe('SelectView:change', (evt) => {
+    const selectedIndex = evt.detail;
+    this.publishCountryDetail(selectedIndex)});
   });
 };
 
-
-
-
-
-
-
-
+Country.prototype.publishCountryDetail = function (selectedIndex) {
+  const selectedCountry = this.text[selectedIndex];
+  PubSub.publish('Countries:selected-country-ready', selectedCountry)
+  console.log(selectedCountry);
+};
 
 
 
